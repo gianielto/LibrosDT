da94579ccb9525fd94a6b61c62ffd97272b23c00
Daniel Torres <danigiotl99@gmail.com>
2026-03-05 11:41:58 -0600

    Corrección: manejar tipo de error desconocido en catch

--- cliente/src/components/ui/productos/ProductoDetalle/ProductoDetalle.tsx
+++ cliente/src/components/ui/productos/ProductoDetalle/ProductoDetalle.tsx
@@ -1,61 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import "./ProductoDetalle.css";
-// interface Book {
-//   nombre: string;
-//   archivo: string;
-//   costo: number;
-//   codigo: string;
-//   stock: number;
-//   descripcion: string;
-// }
-// export default function ProductoDetalle() {
-//   const { codigo } = useParams();
-//   const [book, setBook] = useState<Book | null>(null);
-
-//   useEffect(() => {
-//     const fetchBook = async () => {
-//       try {
-//         const res = await fetch(
-//           `${import.meta.env.VITE_API_URL}/Product/${codigo}`,
-//         );
-//         const data = await res.json();
-//         setBook(data);
-//       } catch (error) {
-//         console.error("Error fetching book:", error);
-//       }
-//     };
-//     fetchBook();
-//   }, [codigo]);
-
-//   if (!book) {
-//     return <div>Cargando...</div>;
-//   }
-//   return (
-//     <div className="detalle-container">
-//       <div className="detalle-imagen">
-//         <img src={`/imagenes/productos/${book.archivo}`} alt={book.nombre} />
-//       </div>
-
-//       <div className="detalle-info">
-//         <h1>{book.nombre}</h1>
-
-//         <p>
-//           <strong>Código:</strong> {book.codigo}
-//         </p>
-//         <p>
-//           <strong>Precio:</strong> ${book.costo}
-//         </p>
-//         <p>
-//           <strong>Stock:</strong> {book.stock}
-//         </p>
-
-//         <h2>Descripción</h2>
-//         <p>{book.descripcion}</p>
-//       </div>
-//     </div>
-//   );
-// }
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./ProductoDetalle.css";
@@ -126,7 +68,13 @@ export default function ProductoDetalle() {
         setError("Error al añadir al carrito: " + data.message);
       }
     } catch (error) {
-      setError("Error al añadir al carrito: " + error.message);
+      if (error instanceof Error) {
+        console.log(error.message);
+        setError("Error al añadir al carrito: " + error.message);
+      } else {
+        console.log("Error desconocido", error);
+        setError("Error al añadir al carrito: " + error);
+      }
     }
   };
 
